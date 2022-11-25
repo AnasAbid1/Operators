fdbd81449cf7187f5978854a4159778465fd7824
Anas Abid <92758772+AnasAbid1@users.noreply.github.com>
2022-11-25 20:30:58 +0500

    Add files via upload

---
+++ Math Libraries/script.js
@@ -0,0 +1,41 @@
+
+        // let value=Math.sqrt(81);
+        // document.write(value);
+
+        // let value=Math.pow(6,2);
+        // document.write(value);
+
+        // let value=Math.min(6,2,35,-10);
+        // document.write(value);
+
+        // let value=Math.random();
+        // document.write(value);
+
+        // let value=Math.abs(-4.4);
+        // document.write(value);
+
+        // let value=Math.floor(3.4);
+        // document.write(value);
+
+        // let value=Math.ceil(3.4);
+        // document.write(value);
+ 
+        // let name = (/Anas/i);
+        // let sentence = "My Name Is Anas";
+        // document.write(name.source);
+        // let a = name.exec(sentence);
+        // document.write (a)
+
+        // let b = sentence.search(name);
+        // document.write(b);
+
+        // let c = sentence.match(name);
+        // document.write(c);
+
+
+        // document.write(a.input);
+        
+        // let c = sentence.replace(name,"Abid");
+        // document.write(c);
+
+        
