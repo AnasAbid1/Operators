75be66f89ee7080141ff8fe71e8bf462ec6921e3
Anas Abid <92758772+AnasAbid1@users.noreply.github.com>
2022-11-30 20:59:51 +0500

    Add files via upload

---
+++ Functions/script.js
@@ -0,0 +1,37 @@
+// alert(5);
+
+
+        // SIMPLE FUNCTION   
+// function simple(){
+//     var a = 1 + 10;
+//     console.log(a)
+// }
+// simple()
+
+        // FUNCTION WITH SINGLE ARGUEMENT   
+// function show(name){
+//     console.log(name)
+// }
+// show("Anas");
+
+        // FUNCTION WITH DOUBLE ARGUEMENT   
+// function show(value1,value2){
+//     console.log(value1,value2)
+// }
+// show("Anas","Abid");
+
+        // FUNCTION WITH DOUBLE ARGUEMENT WITH RETURN KEYWORD
+// function show(value1,value2){
+//     var add = value1 + value2
+// return add;
+// }
+// let abc = show("Anas","Abid");   
+// console.log(abc)
+
+        // ARROW FUNCTION WITH DOUBLE ARGUEMENT WITH RETURN KEYWORD
+// show = (value1,value2)=>{
+//     var add = value1 + value2
+// return add;
+// }
+// let abc = show("Anas","Abid");   
+// console.log(abc)
